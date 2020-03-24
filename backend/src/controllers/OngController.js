2033cb51518783e057388b3525ac3eb559efeb7b
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-24 11:03:43 -0300

    feat(backend): cria Controller para ONGs com métodos cadastro e listagem

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,25 @@
+import crypto from 'crypto';
+
+import connection from '../database/connection';
+
+export default {
+  async index(req, res) {
+    const ongs = await connection('ongs').select('*');
+
+    return res.json(ongs);
+  },
+
+  async create(req, res) {
+    const {
+      name, email, whatsapp, city, uf,
+    } = req.body;
+
+    const id = crypto.randomBytes(8).toString('hex');
+
+    await connection('ongs').insert({
+      id, name, email, whatsapp, city, uf,
+    });
+
+    return res.json({ id });
+  },
+};
