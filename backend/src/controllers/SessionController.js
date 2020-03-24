f43f2854aefd21b924cc91788b5f82c8e2c46736
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-24 12:35:43 -0300

    feat(backend): cria controller para login de ongs

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,14 @@
+import connection from '../database/connection';
+
+
+export default {
+  async create(req, res) {
+    const { id } = req.body;
+
+    const ong = await connection('ongs').where('id', id).select('name').first();
+
+    if (!ong) return res.status(404).json({ error: 'ONG NOT FOUND' });
+
+    return res.json(ong);
+  },
+};
