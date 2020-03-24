37936a0de32e7bec9ab22801faa0498a0a9b82dc
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-24 12:36:16 -0300

    feat(backend): utiliza as rotas de ProfileController e SessionController

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 import ongsRoutes from './ongsRoutes';
 import incidentsRoutes from './incidentsRoutes';
+import profileRoutes from './profileRoutes';
+import sessionRoutes from './sessionRoutes';
 
 const routes = Router();
 
 routes.get('/', (req, res) => res.json('Running Be The Hero API'));
 
 routes.use(ongsRoutes);
+routes.use(sessionRoutes);
+routes.use(profileRoutes);
 routes.use(incidentsRoutes);
 
 export default routes;
