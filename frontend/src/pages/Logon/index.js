a6e79a9a83c8cb9099e8fa660eb9965c3b7b9838
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-26 13:12:49 -0300

    feat(frontend): adiciona conexão com API e login automático

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,32 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.scss';
 
+import api from '../../services/api';
+
 import logoImg from '../../assets/logo.svg';
 import heroesPng from '../../assets/heroes.png';
 
-const Logon = () => (
-  <div className="logon-container">
-    <section className="form">
-      <img src={logoImg} alt="Be The Hero" />
-      <form>
-        <h1>Faça seu logon</h1>
+const Logon = () => {
+  const [id, setId] = useState(localStorage.getItem('ongId'));
 
-        <input placeholder="Seu ID" />
-        <button className="button" type="submit">Entrar</button>
+  const history = useHistory();
 
-        <Link className="back-link" to="/register">
-          <FiLogIn size={16} color="#E02041" />
-          Não tenho cadastro
-        </Link>
-      </form>
-    </section>
+  async function handleLogon(e) {
+    if (e) e.preventDefault();
 
-    <img src={heroesPng} alt="Heroes" />
-  </div>
+    try {
+      const response = await api.post('/sessions', { id });
 
-);
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Falha no login, tente novamente');
+    }
+  }
+
+  useEffect(() => {
+    if (id) history.push('/profile');
+  }, []);
+
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+        <form onSubmit={handleLogon}>
+          <h1>Faça seu logon</h1>
+
+          <input
+            placeholder="Seu ID"
+            required
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <button className="button" type="submit">Entrar</button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={heroesPng} alt="Heroes" />
+    </div>
+
+  );
+};
 
 export default Logon;
