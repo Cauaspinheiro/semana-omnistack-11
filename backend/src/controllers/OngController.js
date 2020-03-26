c922e296c15d14aebc183b9922f2890c76d2ae9e
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-26 12:37:39 -0300

    feat(backend): adiciona crud destroy em Ongs

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -20,6 +20,14 @@ export default {
       id, name, email, whatsapp, city, uf,
     });
 
-    return res.status(204).json({ id });
+    return res.status(201).json({ id });
+  },
+  async destroy(req, res) {
+    const { id } = req.headers;
+
+    await connection('ongs').where('id', id).delete();
+    await connection('incidents').where('ong_id', id).delete();
+
+    return res.status(204).send();
   },
 };
--- backend/src/routes/ongsRoutes.js
+++ backend/src/routes/ongsRoutes.js
@@ -6,6 +6,7 @@ const routes = Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+routes.delete('/ongs', OngController.destroy);
 
 
 export default routes;
